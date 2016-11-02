37ce6f5dc58934e23ec93e82b9232cd9068c8f2d
fspadea <fs279@pvsd.org>
2016-11-02 14:19:47 -0400

    o

--- src/com/company/lesson20quicl.java
+++ src/com/company/lesson20quicl.java
@@ -4,4 +4,8 @@ package com.company;
  * Created by fs279 on 11/2/16.
  */
 public class lesson20quicl {
+    /*
+    static before method signature means no object is needed to use it
+    Class.method
+     */
 }
