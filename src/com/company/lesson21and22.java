613916f2e6122b35fed0772993f95c16ff808754
fspadea <fs279@pvsd.org>
2016-11-03 14:24:18 -0400

    a

---
+++ src/com/company/lesson21and22.java
@@ -0,0 +1,23 @@
+package com.company;
+
+/**
+ * Created by fs279 on 11/3/16.
+ */
+public class lesson21and22 {
+    /*
+    wrapper classes
+    toString
+
+    wrapper classes
+    convert data types into objects
+    Integer ic = 7;
+    int i = 10
+    Integer ii = i;
+    same for all
+
+    public String toString(int i);
+
+    int = 3;
+    String s = "" + j;
+     */
+}
