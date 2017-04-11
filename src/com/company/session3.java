bcaa0cb6c4c97111dded7f7478756d90adcb1e57
fspadea <fs279@pvsd.org>
2017-04-11 14:25:23 -0400

    a

---
+++ src/com/company/session3.java
@@ -0,0 +1,21 @@
+package com.company;
+
+/**
+ * Created by fs279 on 4/11/17.
+ */
+public class session3 {
+    /*
+    don't screw up abstract classes again
+    doesn't copy constructor when extending
+    everything else though
+    no overriding static methods
+    cannot change methods from public to private or vice versa
+    own constructors
+    no accessing private stuff from superclass
+    trump looks down at the mexicans from his wall is a metaphor for class creation hierarchy
+    abstract methods have abstract at beginning
+    interfaces are God's mistake
+    implements
+    no instance variables
+     */
+}
